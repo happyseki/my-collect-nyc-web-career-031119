18def36f8bc632742ea00c06c469f6b8571aee7a
Elaina <elainaseki@gmail.com>
2019-02-16 04:18:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,16 +2,7 @@ def my_collect(collection)
   new = []
   i = 0
  while i < collection.length
- first_name = collection[i].split(" ").first
-   yield(first_name)
-   new << first_name
-   i += 1
- end
- 
- while i < collection.length
- upcase = collection[i].upcase
-   yield(upcase)
-   new << upcase 
+   new << yield(collection[i])
    i += 1
  end
  return new
