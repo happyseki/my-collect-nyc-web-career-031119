8cb69555f6ecf8d6d624e57317f58ceec1a2c7c8
Elaina <elainaseki@gmail.com>
2019-02-16 04:13:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,9 @@ def my_collect(collection)
   new = []
   i = 0
  while i < collection.length
-   yield(new << collection[i])
+ first_name = collection[i].split(" ").first
+   yield(first_name)
+   new << first_name
    i += 1
  end
  return new
